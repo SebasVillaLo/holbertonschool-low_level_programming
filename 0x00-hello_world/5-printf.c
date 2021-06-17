75719b921a94f1afe0ea7b785866f2825d2c1d2f
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-17 14:02:44 -0700

    Add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entrada al programa
+ **
+ *Return: siempre 0 (Si funciona)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
