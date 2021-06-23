1ff1db444d68ff741d82d390f29ed5b5f8fe6b3d
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-22 21:46:39 -0700

    Add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @x: character
+ * @y: character
+ * Return: 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
