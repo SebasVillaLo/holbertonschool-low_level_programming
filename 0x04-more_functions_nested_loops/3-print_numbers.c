b8d30fef54db7086fcc138c0f2ed9c9af4093c7a
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-24 16:09:40 -0700

    add task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - Enter code
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 0)
+	{
+		_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
+}
