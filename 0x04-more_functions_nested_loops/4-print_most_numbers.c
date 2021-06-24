d9920d1ba891bbb62aead214eabba9e39778e1c3
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-24 16:15:01 -0700

    add task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Enter code
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
+}
