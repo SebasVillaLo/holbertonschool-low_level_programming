4a7631cb447da908ceba7482c04608cf9c61e02e
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-24 20:34:44 -0700

    add tasks

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_line - Enter code
+ * @n: character
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Enter code
+ * @n: character
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			for (i = 1; i <= j; i++)
+			{
+				if (i != 1)
+					_putchar(' ');
+				if (1 == 1)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	else
+		_putchar('\n')
+}
