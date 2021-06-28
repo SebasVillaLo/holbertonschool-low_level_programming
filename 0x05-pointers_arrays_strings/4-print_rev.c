f2b62b46a5241c5a625182bc9bb0cc70c9f3aaa7
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-28 14:42:52 -0700

    add task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,17 +7,13 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
+	int n;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
+	while (s[n] != '\0')
+		n++;
+	n--;
+	for (i = n; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
-
 }
