62b3d4f343d7f14640de26a61b37fc0ad3d7158f
SebasVillaLo <villalopezsebastian@gmail.com>
2021-06-29 13:07:20 -0700

    add task 5 and holbi

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rev_string - check the code for Holberton School students.
+ * @s: character
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i, j = 0;
+	char k[500];
+
+	while (*(s + i))
+	{
+		*(k + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(k + j);
+		j++;
+		i--;
+	}
+}
