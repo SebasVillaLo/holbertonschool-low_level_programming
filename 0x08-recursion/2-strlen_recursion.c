760f997fb3834721913692b8842f06e5498ade86
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-07 14:42:05 -0700

    add task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Prototipo para resolver el problema
+ * @s: Character
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
+}
