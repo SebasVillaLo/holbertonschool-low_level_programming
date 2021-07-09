054d7025191648247a18100ebe777b37b7f367e5
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-09 15:48:57 -0700

    add task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints buffer in hexa
+ * @argc: Character
+ * @argv: Character
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int r1, r2;
+
+	r1 = r2 = 0;
+	if (argc == 3)
+	{
+		r1 = atoi(argv[1]);
+		r2 = atoi(argv[2]);
+		printf("%d\n", r1 * r2);
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
