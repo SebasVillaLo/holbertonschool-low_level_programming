646854d8f6b467aa243fa5d9288fede7deacf490
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-09 15:48:04 -0700

    add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints buffer in hexa
+ * @argc: Character
+ * @argv: Character
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc--; /*added to pass checks*/
+	printf("%s\n", argv[0]);
+	return (0);
+}
