dccc9c0b047d9d40cc85a32bdd9073ef419f858c
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-14 08:18:17 -0700

    add task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,30 +7,29 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int j, i;
-	int **p;
+	int i, j, **p;
 
-	i = j = 0;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = (int **)malloc(sizeof(int *) * height);
 	if (p == NULL)
-	{
-		free(p);
 		return (NULL);
-	}
-	if (height < 1)
-		return (NULL);
-	p = (int **)malloc(height * sizeof(p));
+
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(width * sizeof(int));
-		if (p[i] == NULL)
+		*(p + i) = (int *)malloc(sizeof(int) * width);
+
+		if (*(p + i) == NULL)
 		{
-			for (j = 0; j < i; j++)
-				free(p[j]);
+			for (; i >= 0; i--)
+				free(*(p + i));
 			free(p);
 			return (NULL);
 		}
+
 		for (j = 0; j < width; j++)
-			p[i][j] = 0;
+			*(*(p + i) + j) = 0;
 	}
 	return (p);
 }
