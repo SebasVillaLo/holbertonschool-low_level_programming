41eac3ec0e1e9babbd1c36c712330c96641abaac
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-16 21:10:46 -0700

    add task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,30 +7,25 @@
  * @n: character
  * Return: pointer
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int s1count, s2count, sizeBuffer, i;
+	char *concat;
+	unsigned int len = n, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (s1count = 0; s1[s1count]; s1count++)
-		;
-	for (s2count = 0; s2[s2count]; s2count++)
-		;
-	s2count > n ? (s2count = n) : (n = s2count);
-
-	sizeBuffer = s1count + s2count + 1;
-
-	p = malloc(sizeBuffer * sizeof(char));
-
-	if (p == NULL)
+	for (i = 0; s1[i]; i++)
+		len++;
+	concat = malloc(sizeof(char) * (len + 1));
+	if (concat == NULL)
 		return (NULL);
-	for (i = 0; i < sizeBuffer - 1; i++)
-		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
-	p[sizeBuffer] = '\0';
-	return (p);
+	len = 0;
+	for (i = 0; s1[i]; i++)
+		concat[len++] = s1[i];
+	for (i = 0; s2[i] && i < n; i++)
+		concat[len++] = s2[i];
+	concat[len] = '\0';
+	return (concat);
 }
