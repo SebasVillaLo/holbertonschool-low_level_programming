afd77904ca0a0759117588ae85832fd27c98b1b3
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-19 14:50:26 -0700

    add complementos

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+#Hola, Esto es para saber como funcionan las struct, espero te sea util
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - estructura
+ * @name: nombre del perro
+ * @age: edad del perro
+ * @owner: propietario del perro
+ *
+ * Description: datos sobre el perro
+ */
+struct dog
+{
+	char name[];
+	float age;
+	char owner[];
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
