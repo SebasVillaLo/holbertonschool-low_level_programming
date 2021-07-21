c7f5a525bc83c4dc4945d30d0e48b45afa80764c
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-21 14:21:39 -0700

    add task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - entrada de la funcion
+ * @name: el nombre
+ * @f: character
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
