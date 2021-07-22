cd36927ee3411398e46028877fc34cf3486d70af
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-21 21:16:34 -0700

    add task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+* op_add - estructura para la operacion
+* @a: numero
+* @b: numero
+* Return: Devuelve la operacion
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - estructura para la operacion
+* @a: numero
+* @b: numero
+* Return: Devuelve la operacion
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mod - estructura para la operacion
+* @a: numero
+* @b: numero
+* Return: Devuelve la operacion
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+* op_div - estructura para la operacion
+* @a: numero
+* @b: numero
+* Return: Devuelve la operacion
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mul - estructura para la operacion
+* @a: numero
+* @b: numero
+* Return: Devuelve la operacion
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
