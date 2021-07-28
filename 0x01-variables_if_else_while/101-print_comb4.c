bdda1a01ccc19e5e276a774b13e6b158a8c5b224
SebasVillaLo <85509028+SebasVillaLo@users.noreply.github.com>
2021-07-28 15:08:53 -0500

    add task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-int i, j, k;
+int j, i, k;
 
 for (i = '0'; i <= '9'; i++)
 {
-	for (j = i + 1; j <= '9'; j++)
+	for (k = i + 1; k <= '9'; k++)
 	{
-		for (k = j + 1; k <= '9'; k++)
+		for (j = k + 1; j <= '9'; j++)
 		{
 			putchar(i);
-			putchar(j);
 			putchar(k);
+			putchar(j);
 			if (i != '7' || j != '8' || k != '9')
 			{
 				putchar(',');
