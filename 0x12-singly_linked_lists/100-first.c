e64c56f75a658f396fb8a609f097d8af161c5d6b
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-30 23:46:15 -0500

    tarea avanzada 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void imprimir(void) __attribute__ ((constructor));
+/**
+* imprimir - ...
+*/
+void imprimir(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
