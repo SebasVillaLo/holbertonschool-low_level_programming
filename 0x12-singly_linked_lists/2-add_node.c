325a0a66d2ee9f081d8821b051f886935b4c8d5f
SebasVillaLo <villalopezsebastian@gmail.com>
2021-07-30 21:48:58 -0500

    add tarea 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_node - agregar nuevo nodo
+ * @*head: doble puntero
+ * @str: const
+ * Return: head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_nodo;
+	int i = 0;
+
+	if (new_nodo == NULL)
+		return (NULL);
+
+	new_nodo = malloc(sizeof(list_t));
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	new_nodo->str = strdup(str);
+	new_nodo->len = i;
+	new_nodo->next = (*head);
+	(*head) = new_nodo;
+
+	return (*head);
+}
