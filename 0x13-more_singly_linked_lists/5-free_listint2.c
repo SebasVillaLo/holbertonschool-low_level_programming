b6e2966e58d6fe96654b158e8a0cf7377917bf99
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-03 15:40:23 -0500

    add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - liberar memoria
+ * @head: puntero del puntero de la cabeza de la lista
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	if (head == NULL && *head == NULL)
+	{
+		return;
+	}
+
+	while ((*head)->next != NULL)
+	{
+		aux = (*head)->next;
+		free(*head);
+		*head = aux;
+	}
+	free(*head);
+	*head = NULL;
+}
