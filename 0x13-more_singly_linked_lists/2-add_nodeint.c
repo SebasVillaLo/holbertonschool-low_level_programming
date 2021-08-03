a1ec87a505e4c843d9d667327a2ec7016b936f50
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-03 14:35:22 -0500

    arreglos

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,17 +8,23 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *N_nodo;
-	int i = 0;
-	const int *p = &n;
+
+	if (head == NULL || n == 0)
+	{
+		return (NULL);
+	}
 
 	N_nodo = malloc(sizeof(listint_t));
 
 	if (N_nodo == NULL)
 	{
-	return (NULL);
+		return (NULL);
 	}
+  /* asigna el valor de n del main a la n de estructura */
 	N_nodo->n = n;
+  /* asigna head como nodo siguiente */
 	N_nodo->next = (*head);
+  /* asigna la cabecera como el nuevo nodo */
 	(*head) = N_nodo;
 
 	return (*head);
