6cee7af0db4442d850ab4a46deefb4df3ec4fabc
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-03 15:47:32 -0500

    add task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	if (head == NULL && *head == NULL)
-	{
+	if (head == NULL || *head == NULL)
 		return;
-	}
 
 	while ((*head)->next != NULL)
 	{
