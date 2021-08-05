fc7268eef4c127e0d477f897de13a1c729cdc60d
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-05 16:51:45 -0500

    add task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - Convert binary a int
+ * @b: const char
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int r = 0;
+	int i = 0, bs = 1;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[i + 1])
+	{
+		if (b[i] != '\0' && b[1] != '1')
+		{
+			return (0);
+		}
+		while (i >= 0)
+		{
+			r = r + ((b[i] - '0') * bs);
+			bs = bs * 2;
+			i--;
+		}
+		return (r);
+	}
+}
