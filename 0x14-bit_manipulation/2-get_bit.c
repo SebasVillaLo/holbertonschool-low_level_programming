40e04bb052ca456181757dba8430bdb3cfcdac31
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-05 20:31:52 -0500

    add task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_bit - obtener 
+ * @n: unsigned long int
+ * @index: unsigned int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (sizeof(n) * 8 < index)
+	{
+		return (-1);
+	}
+
+	n >>= index;
+	if ((n & 1) == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
