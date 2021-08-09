846c95f80c11a06bfb0e49ef9aee08681858aa29
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-09 16:17:31 -0500

    add el write en el if

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,31 +7,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ar = 0, wf = 0, len = 0;
+	int ar = 0, len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	ar = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	ar = open(filename, O_CREAT | O_RDWR | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (ar == -1)
 	{
 		return (-1);
 	}
-	if (!ar)
+	if (!text_content)
 	{
 		while (text_content[len])
 		{
 			len++;
 		}
+		write(ar, text_content, len);
 	}
-	write(ar, text_content, len);
-	if (wf == -1)
-	{
-		return (-1);
-	}
-
+	
 	close(ar);
 	return (1);
 }
