e781df4f3034a59e79ccbc02d2c42776f1d7ffae
SebasVillaLo <villalopezsebastian@gmail.com>
2021-08-09 16:25:02 -0500

    quite permiso que sobraba

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	ar = open(filename, O_RDWR | O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	ar = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (ar == -1)
 	{
 		return (-1);
