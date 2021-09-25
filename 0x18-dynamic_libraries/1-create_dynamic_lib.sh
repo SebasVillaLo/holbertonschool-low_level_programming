65d350617eea06d517cfb4a3ba27bb14f037bd0c
SebasVillaLo <villalopezsebastian@gmail.com>
2021-09-25 15:45:47 -0500

    add task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc *.c -c -fPIC | gcc *.o -shared -o liball.so | export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH 
+gcc -Wall -Werror -Wextra -pedantic *.c -c -fPIC
+gcc -Wall -Werror -Wextra -pedantic *.o -shared -o liball.so
+export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH 
