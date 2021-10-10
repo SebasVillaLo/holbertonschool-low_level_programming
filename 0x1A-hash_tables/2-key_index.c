4600d8d1be12cbe60645e359d9a78d9696a8de93
SebasVillaLo <villalopezsebastian@gmail.com>
2021-10-10 12:11:13 -0500

    add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * key_index - implementation of the djb2 algorithm
+ * @key: char
+ * @size: int
+ * Return: hash value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idx;
+
+	if (key == 0 || size == 0)
+		return (0);
+	idx = djb2(key) % size;
+	return (idx);
+}
