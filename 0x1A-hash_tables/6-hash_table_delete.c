f06384c72fd5c1db8e69a178bda5ea324874f0e9
SebasVillaLo <villalopezsebastian@gmail.com>
2021-10-10 17:27:50 -0500

    add task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+/**
+  * hash_table_delete - Delete
+  * @ht: hash table
+  * Return: 0
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *aux = NULL;
+
+	if (ht && ht->size && ht->array)
+	{
+		for (i = 0; i < ht->size; ++i)
+		{
+			node = ht->array[i];
+
+			if (node)
+			{
+				if (node->next)
+				{
+					node = node->next;
+					while (node)
+					{
+						aux = node;
+						node = node->next;
+						free(aux->key);
+						free(aux->value);
+						free(aux);
+					}
+				}
+
+				node = ht->array[i];
+				if (node->key && node->value)
+				{
+					free(node->key);
+					free(node->value);
+				}
+			}
+
+			free(node);
+		}
+
+		free(ht->array);
+		free(ht);
+	}
+}
